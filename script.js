96ad968afc359f29d3095f5cd4bdc4c745814c74
ceceliacshelley <59037456+ceceliacshelley@users.noreply.github.com>
2020-02-19 18:22:07 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,40 @@
-const wantGame = prompt(`Welcome, would you like to play a game? [yes] or [no]
-`);
+//startGame function//
+
+// const wantGame = prompt(`Welcome, would you like to play a game? [yes] or [no]
+// `);
+
+const wantGame = prompt(
+  `Welcome, would you like to play a game? [yes] or [no]`
+);
+let attackOrQuit = "attack";
+
+function startGame() {
+  while (user.health > 0 && user.wins < 3 && attackOrQuit === "attack") {
+    console.log(user.name + " has " + user.health + " health left.");
+    console.log(
+      "Grant the mighty chicken has " + grant.health + " health left."
+    );
+    grant.health -= getDamage(); // grant.health = grant.health - getDamage();//
+    user.health -= getDamage();
+    if (grant.health <= 0) {
+      user.wins += 1; //user.wins = user.wins + 1    user.wins++//
+      alert("the user now has " + user.wins + " wins.");
+      grant.health = 10;
+    }
+    attackOrQuit = prompt(`Would you like to attack or quit?`);
+  }
+
+  if (user.health <= 0) {
+    alert("Grant has won.");
+  } else {
+    alert(user.name + " has won the game.");
+  }
+}
 
 // const userHealth = 40;
 // const grantHealth = 10;
+
+//startCombat function//
 
 const user = {
   health: 40,
@@ -16,6 +48,8 @@ const grant = {
 
 user.name = prompt(`What is your name?`);
 
+const playOrQuit = prompt(`Would you like to attack or quit?`);
+
 function getDamage() {
   return Math.floor(Math.random() * 2) + 1;
 }
@@ -25,24 +59,7 @@ function getDamage() {
 // };
 
 if (wantGame === "yes") {
-  while (user.health > 0 && user.wins < 3) {
-    console.log(user.name + " has " + user.health + " health left.");
-    console.log(
-      "Grant the mighty chicken has " + grant.health + " health left."
-    );
-    grant.health -= getDamage(); // grant.health = grant.health - getDamage();//
-    user.health -= getDamage();
-    if (grant.health <= 0) {
-      user.wins += 1; //user.wins = user.wins + 1    user.wins++//
-      alert("the user now has " + user.wins + " wins.");
-      grant.health = 10;
-    }
-  }
-  if (user.health <= 0) {
-    alert("Grant has won.");
-  } else {
-    alert(user.name + " has won the game.");
-  }
+  startGame();
 }
 
 // || means "or" condition
